f149ef79b7ba8db680d40cc7aa15a75fa4ef9d74
zkfmapf123 <zkfmapf123@naver.com>
2024-04-27 16:09:19 +0900

    Update sg

--- sg.tf
+++ sg.tf
@@ -43,10 +43,11 @@ resource "aws_security_group" "ecs_sg" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    security_groups = [aws_security_group.alb_sg.id]
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # security_groups = [aws_security_group.alb_sg.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
